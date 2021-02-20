687148cb78e69d258b787d83166c332134d21511
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-20 22:03:39 +0500

    FIx template generator: add run all file, fix rust gen

--- generate_template.py
+++ generate_template.py
@@ -12,3 +12,11 @@ CSharpTemplate(path + task_name + "/", "solution")
 HaskellTemplate(path + task_name + "/", "solution")
 PythonTemplate(path + task_name + "/", "solution")
 RustTemplate(path + task_name + "/", "solution", True)
+
+run_all_template_file = open("templates/run_all_template.bat", "r")
+content = run_all_template_file.read()
+run_all_template_file.close()
+
+f = open(path + task_name + "/" + "run_all.bat", "w")
+f.write(content)
+f.close()
--- templates/generate_rust_template.py
+++ templates/generate_rust_template.py
@@ -3,4 +3,4 @@ from templates.template import Template
 
 class RustTemplate(Template):
     def create_template(self, filename):
-        os.system(f"cargo init \"{filename}\"")
+        os.system(f"cargo init \"{filename}\" --vcs none")
