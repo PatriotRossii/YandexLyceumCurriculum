3008fd6d5c75c117b23394f137ec883d8677afec
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-23 11:34:44 +0500

    Выполнена задача "Исправление ошибок в is_prime()"

---
+++ src/Second course/Цели и подходы к тестированию. Создание «самодельных» тестов (без библиотек)/Исправление ошибок в is_prime()/main.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for divisor in range(2, int(n ** 0.5)):
+        if n % divisor == 0:
+            return False
+    return True
+
+
+n = int(input())
+print("YES" if is_prime(n) else "NO")
