35c16b31202c088a1068b05c03a8f73db40d5030
PatriotRossii <patriotrossii2019@mail.ru>
2021-03-03 17:05:48 +0500

    Решена задача "Добавляем капитана"

---
+++ src/Second course/WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/main.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/people.db")
+
+    captain = User()
+    captain.surname = "Scott"
+    captain.name = "Ridley"
+    captain.age = 21
+    captain.position = "captain"
+    captain.speciality = "research engineer"
+    captain.address = "module_1"
+    captain.email = "scott_chief@mars.org"
+
+    colonist = User()
+    colonist.surname = "Washington"
+    colonist.name = "George"
+    colonist.age = 25
+    colonist.position = "navigator"
+    colonist.speciality = "engineer"
+    colonist.address = "module_2"
+    colonist.email = "washigton_george@mars.org"
+
+    doctor = User()
+    doctor.surname = "Smith"
+    doctor.name = "Will"
+    doctor.age = 30
+    doctor.position = "doctor"
+    doctor.speciality = "neurobiologist"
+    doctor.address = "module_3"
+    doctor.email = "will_smith@mars.org"
+
+    lead_research = User()
+    lead_research.surname = "Wright"
+    lead_research.name = "Apollo"
+    lead_research.age = 21
+    lead_research.position = "scientist"
+    lead_research.speciality = "lead researcher"
+    lead_research.address = "module_4"
+    lead_research.email = "apollo_wright@mars.org"
+
+    db_sess = db_session.create_session()
+
+    db_sess.add(captain)
+    db_sess.add(colonist)
+    db_sess.add(doctor)
+    db_sess.add(lead_research)
+
+    main()
