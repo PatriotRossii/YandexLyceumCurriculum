da9f7a895979794a39c1577a0ab291d905fb0acd
PatriotRossii <patriotrossii2019@mail.ru>
2021-03-06 16:22:41 +0500

    Решена задача "Обработчик формы авторизации"

---
+++ src/Second course/WEB. Flask-sqlalchemy/Обработчик формы авторизации/main.py
@@ -0,0 +1,25 @@
+from flask import app, render_template, redirect
+from flask_login import login_user
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+    return render_template('login.html', title='Авторизация', form=form)
