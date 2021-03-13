2cb1e66314793d1c6a65aec274138b87800afafb
PatriotRossii <patriotrossii2019@mail.ru>
2021-03-13 15:31:06 +0500

    Решена задача "Получение одной работы"

---
+++ src/Second course/WEB. REST-API. Делаем простое Rest-api/Получение одной работы/jobs_api.py
@@ -0,0 +1,24 @@
+import flask
+from flask import jsonify
+
+from . import db_session
+from .jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_job(job_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(job_id)
+    if not jobs:
+        return jsonify({"error": "Not found"})
+    return jsonify(
+        {
+            'jobs': jobs.to_dict()
+        }
+    )
