c14e0d35d9a8e965cfe47e5c2a227c82725a468a
PatriotRossii <patriotrossii2019@mail.ru>
2021-03-13 15:31:27 +0500

    Решена задача "Удаление работы"

---
+++ src/Second course/WEB. REST-API. Делаем простое Rest-api/Удаление работы/jobs_api.py
@@ -0,0 +1,23 @@
+import flask
+from flask import jsonify
+
+from . import db_session
+from .jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['DELETE'])
+def delete_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+
+    if not jobs:
+        return jsonify({'error': 'Not found'})
+    db_sess.delete(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
